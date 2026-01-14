e5dab9aff3f28595f0e347f3b5c701f38c7fd1d3
suibaka <suibaka@gmail.com>
2026-01-14 14:42:01 +0900

    Use blink.cmp

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -5,8 +5,6 @@ vim.lsp.enable({
 })
 
 
--- keybindings memo
--- <C-w>d : show diagnostic message
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("my.lsp", {}),
   callback = function(args)
@@ -23,29 +21,29 @@ vim.api.nvim_create_autocmd("LspAttach", {
         { buffer = buf, desc = "Show hover documentation" })
     end
 
-    if client:supports_method("textDocument/completion") then
-      -- Use CTRL-space to trigger LSP completion manually.
-      vim.keymap.set('i', '<C-Space>', function()
-        vim.lsp.completion.get()
-      end)
+    -- if client:supports_method("textDocument/completion") then
+    --   -- Use CTRL-space to trigger LSP completion manually.
+    --   vim.keymap.set('i', '<C-Space>', function()
+    --     vim.lsp.completion.get()
+    --   end)
 
-      if client.server_capabilities.completionProvider then
-        -- Trigger autocompletion on every keypress. May be slow!
-        local tmp_chars = {}; for i = 32, 126 do table.insert(tmp_chars, string.char(i)) end
-        local trigger_chars = tmp_chars
-        local additional_chars = { '.', ':', '->', '::' }
-        for _, char in ipairs(additional_chars) do
-          table.insert(trigger_chars, char)
-        end
-        client.server_capabilities.completionProvider.triggerCharacters = trigger_chars
-      end
+    --   if client.server_capabilities.completionProvider then
+    --     -- Trigger autocompletion on every keypress. May be slow!
+    --     local tmp_chars = {}; for i = 32, 126 do table.insert(tmp_chars, string.char(i)) end
+    --     local trigger_chars = tmp_chars
+    --     local additional_chars = { '.', ':', '->', '::' }
+    --     for _, char in ipairs(additional_chars) do
+    --       table.insert(trigger_chars, char)
+    --     end
+    --     client.server_capabilities.completionProvider.triggerCharacters = trigger_chars
+    --   end
 
-      vim.lsp.completion.enable(true, client.id, args.buf, {
-        autotrigger = true,
-        convert = function (item)
-          return { abbr = item.label:gsub('%b()', '') }
-        end,
-      })
-    end
+    --   vim.lsp.completion.enable(true, client.id, args.buf, {
+    --     autotrigger = true,
+    --     convert = function (item)
+    --       return { abbr = item.label:gsub('%b()', '') }
+    --     end,
+    --   })
+    -- end
   end,
 })
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -107,7 +107,7 @@ return {
     },
     {
         'mrcjkb/rustaceanvim',
-        version = '^5', -- Recommended
+        version = '^6', -- Recommended
         lazy = false, -- This plugin is already lazy
     }
 }
